0d3bcd551ef03bb102a402caa89c081ef636c5f1
pbaffiliate4 <pbaffiliate4@gmail.com>
2015-03-10 10:32:28 +0200

    dsfsdfsdf

--- path1/scripts/TestA.js
+++
@@ -1,6 +0,0 @@
-/** Script ACLs do not delete 
- read=nobo 
-write=nobo
-execute=authentic
-  **/ 
- return "0"   							
--- path1/scripts/TestingACLs.js
+++
@@ -1,8 +0,0 @@
-/** Script ACLs do not delete 
- read=nobo 
-write=nobo
-execute=authentica 
-  **/ 
-
- 
- return "1"   							
