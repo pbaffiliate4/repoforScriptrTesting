14a99aa18aa3e236f64fdc15f286f12653eefea9
pbaffiliate4 <pbaffiliate4@gmail.com>
2015-03-13 09:52:19 +0200

    hgjgjh

--- path1/scripts/Blockl1.js
+++ path1/scripts/Blockl1.js
@@ -3,6 +3,7 @@
 write=nobody
 execute=authenticated 
   **/ 
+ 
  /*#*beginblockly*#*<xml xmlns="http://www.w3.org/1999/xhtml"><block type="controls_if" id="8" inline="false" x="276" y="27"></block></xml>*#*#*/
 if (x==100) {
 }
