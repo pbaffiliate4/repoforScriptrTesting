5c6dae9b591aa2fc4dd2e512ad71f949c8f228dc
pbaffiliate4 <pbaffiliate4@gmail.com>
2015-03-13 09:43:45 +0200

    zcvcvcxv

---
+++ path1/scripts/Blockl1.js
@@ -0,0 +1,9 @@
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=authenticated 
+  **/ 
+ /*#*beginblockly*#*<xml xmlns="http://www.w3.org/1999/xhtml"><block type="controls_if" id="8" inline="false" x="276" y="27"></block></xml>*#*#*/
+if (false) {
+}
+   							
