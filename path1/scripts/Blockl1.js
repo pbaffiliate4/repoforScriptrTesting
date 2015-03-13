b7cdcca295e84e2a2a8039a786461278e71a9761
pbaffiliate4 <pbaffiliate4@gmail.com>
2015-03-13 09:51:04 +0200

    Update Blockl1.js

--- path1/scripts/Blockl1.js
+++ path1/scripts/Blockl1.js
@@ -4,6 +4,6 @@ write=nobody
 execute=authenticated 
   **/ 
  /*#*beginblockly*#*<xml xmlns="http://www.w3.org/1999/xhtml"><block type="controls_if" id="8" inline="false" x="276" y="27"></block></xml>*#*#*/
-if (false) {
+if (x==100) {
 }
    							
