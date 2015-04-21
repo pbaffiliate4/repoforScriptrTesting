fac91ba0ecde3947bbc3cfe0b870ac2f298b8730
pbaffiliate4 <pbaffiliate4@gmail.com>
2015-04-21 14:45:55 +0300

    dsfsdfdf

--- path1/scripts/opop34.js
+++
@@ -1,9 +0,0 @@
-/** Script ACLs do not delete 
- read=nobody 
-write=nobody
-execute=authenticated 
-  **/ 
- /*#*beginblockly*#*<xml xmlns="http://www.w3.org/1999/xhtml"><block type="controls_whileUntil" id="1" inline="false" x="378" y="37"><field name="MODE">WHILE</field></block></xml>*#*#*/
-while (false) {
-}
-   							
