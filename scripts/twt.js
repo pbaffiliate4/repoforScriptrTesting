6d2b136635383f8775e1dbddc9fb5999f876cbe5
Afaf Zakhya <pbaffiliate4@gmail.com>
2016-01-22 11:35:01 +0200

    g

--- scripts/twt.js
+++ scripts/twt.js
@@ -3,6 +3,6 @@
 write=nobody
 execute=authenticated 
   **/ 
- /*#*beginblockly*#*<xml xmlns="http://www.w3.org/1999/xhtml"><block type="scriptr_tweet" id="1" inline="true" x="1" y="61"><value name="tweetString"><block type="text" id="2"><field name="TEXT">sunday afternoon</field></block></value></block></xml>*#*#*/
-tweet ('sunday afternoon');
+ /*#*beginblockly*#*<xml xmlns="http://www.w3.org/1999/xhtml"><block type="scriptr_tweet" id="1" inline="true" x="154" y="39"><value name="tweetString"><block type="text" id="2"><field name="TEXT">sunday zxczxcxzc afternoon</field></block></value></block></xml>*#*#*/
+tweet ('sunday zxczxcxzc afternoon');
 			
