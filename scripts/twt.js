02bbc856eba36cb64d8e0c142a49b2d346123576
Afaf Zakhya <pbaffiliate4@gmail.com>
2016-01-22 09:39:54 +0200

    twt

---
+++ scripts/twt.js
@@ -0,0 +1,8 @@
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=authenticated 
+  **/ 
+ /*#*beginblockly*#*<xml xmlns="http://www.w3.org/1999/xhtml"><block type="scriptr_tweet" id="1" inline="true" x="1" y="61"><value name="tweetString"><block type="text" id="2"><field name="TEXT">sunday afternoon</field></block></value></block></xml>*#*#*/
+tweet ('sunday afternoon');
+			
