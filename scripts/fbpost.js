8633aae298a5c3955f4434a87efa530a892ee558
Afaf Zakhya <pbaffiliate4@gmail.com>
2016-02-15 14:47:09 +0200

    e

---
+++ scripts/fbpost.js
@@ -0,0 +1,7 @@
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=authenticated 
+  **/ 
+ /*#*beginblockly*#*<xml xmlns="http://www.w3.org/1999/xhtml"><block id="76" type="scriptr_return" inline="false" x="61" y="88"><value name="return"><block id="46" type="scriptr_fb_post" inline="true"><value name="message"><block id="122" type="text"><field name="TEXT">even flow</field></block></value></block></value></block></xml>*#*#*/
+return facebookPost ('even flow')   							
