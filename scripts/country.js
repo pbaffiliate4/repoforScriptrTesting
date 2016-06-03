fedb10d77524bfd18893a00342e2a73cf733a123
Afaf Zakhya <pbaffiliate4@gmail.com>
2016-06-03 14:41:44 +0300

    rews

---
+++ scripts/country.js
@@ -0,0 +1,14 @@
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=anonymous 
+  **/ 
+ var country = [ 
+    ['Language', 'Speakers (in millions)'],
+    ['German',  5.85],
+    ['French',  1.96],
+    ['Italian', 0.316],
+    ['Romansh', 0.0791]
+];
+
+return country;			
